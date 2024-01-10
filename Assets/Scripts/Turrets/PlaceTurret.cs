963dde61db7b44fb896aa797dbe5f03b5323df22
shenanigan11 <enzo2dessaint@gmail.com>
2024-01-10 15:09:35 +0100

    can't block way

--- Assets/Scripts/Turrets/PlaceTurret.cs
+++ Assets/Scripts/Turrets/PlaceTurret.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 using static UnityEngine.GraphicsBuffer;
@@ -16,6 +17,8 @@ public class PlaceTurret : MonoBehaviour
     public GameObject Objective;
     private bool putTurret;
 
+    private GameObject lastTurret;
+
     private void Awake()
     {
         if(Instance == null) { Instance = this; }
@@ -33,6 +36,10 @@ public class PlaceTurret : MonoBehaviour
     {
         MakePreview();
         Debug.Log(IsAlreadyAPath());
+        if (!IsAlreadyAPath())
+        {
+            Destroy(lastTurret);
+        }
     }
 
     public void MakePreview()
@@ -46,7 +53,7 @@ public class PlaceTurret : MonoBehaviour
             {
                 if(!preview.activeSelf)
                     preview.SetActive(true);
-                preview.gameObject.transform.position = hit.point;
+                preview.gameObject.transform.position = hit.point + new Vector3(0,2,0);
 
                 if(putTurret && turret != null) 
                 {
@@ -54,15 +61,11 @@ public class PlaceTurret : MonoBehaviour
                     {
                         GameObject go = Instantiate<GameObject>(turret.turret, hit.point, new Quaternion(0, 0, 0, 0), ParentTurret.transform);
                         go.GetComponentInChildren<TurretController>().info = turret;
-                        if (IsAlreadyAPath())
-                        {
-                            Destroy(go);
-                            Economie.Instance.AddGolds(turret.cost);
-                        }
+                        lastTurret = go;
                     }
 
-                    //putTurret = false;
-                    //turret = null;
+                    putTurret = false;
+                    turret = null;
                 }
             }
             else
@@ -84,10 +87,19 @@ public class PlaceTurret : MonoBehaviour
     public bool IsAlreadyAPath()
     {
         //NavMeshPath path = new();
+        //EnnemieSpawn.GetComponent<NavMeshAgent>().CalculatePath(Objective.transform.position, path);
         //return EnnemieSpawn.GetComponent<NavMeshAgent>().CalculatePath(Objective.transform.position,path);
+        //NavMeshPath path = new NavMeshPath();
+        //if (NavMesh.CalculatePath(EnnemieSpawn.transform.position, Objective.transform.position, NavMesh.AllAreas, path))
+
+        NavMesh.SamplePosition(EnnemieSpawn.transform.position, out NavMeshHit hitA, 10f, NavMesh.AllAreas);
+        NavMesh.SamplePosition(Objective.transform.position, out NavMeshHit hitB, 10f, NavMesh.AllAreas);
+
         NavMeshPath path = new NavMeshPath();
-        if (/*NavMesh.CalculatePath(EnnemieSpawn.transform.position, Objective.transform.position, NavMesh.AllAreas, path)*/EnnemieSpawn.GetComponent<NavMeshAgent>().CalculatePath(Objective.transform.position, path))
+        if (NavMesh.CalculatePath(hitA.position, hitB.position, NavMesh.AllAreas , path))
+        //if(EnnemieSpawn.GetComponent<NavMeshAgent>().CalculatePath(hitB.position, path))
         {
+            
             bool isvalid = true;
             if (path.status != NavMeshPathStatus.PathComplete)
                 isvalid = false;
